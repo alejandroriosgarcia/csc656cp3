ef0e0021a971837bdbc8b66392618be9109652b9
alejandroriosgarcia <alxtrg@comcast.net>
2024-05-03 17:21:11 -0700

    updated without tempyarr

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,23 +8,10 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
-
-   // Temp array to hold result of Ax
-   double tempYarr[n];
-
-   for (int i = 0; i < n; i++) {
-      tempYarr[i] = 0.0;
-   }
-
    // Do A * X portion of operation
    for (int i = 0; i < n; i++) {
       for (int j = 0; i < n; i++) {
-         tempYarr[i] += A[i * n + j] * x[j];
+         y[i] += A[i * n + j] * x[j];
       }
-   }
-
-   // Add previous result to Y
-   for (int i = 0; i < n; i++) {
-      y[i] += tempYarr[i];
    }
 }
